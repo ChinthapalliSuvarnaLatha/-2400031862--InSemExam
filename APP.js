fb19f6a9c3733e09ac2dfc72a8e6baa3bbd900bd
2400031862@kluniversity.in <2400031862@kluniversity.in>
2025-10-16 10:39:05 +0530

    main

---
+++ APP.js
@@ -0,0 +1,9 @@
+function greet() {
+  return "Hello from Alice (feature1)!";
+}
+module.exports = { greet };
+function greet() {
+  return "Hello from Bob (feature2)!";
+}
+module.exports = { greet };
+
